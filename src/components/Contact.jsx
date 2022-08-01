9a50e5a28444b930ba5e97e0737afffab1160a49
Christina Webber <chrisswebb@yahoo.com>
2022-08-01 15:41:10 -0500

    connected backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-export const REACT_APP_BACKEND_URL='https://renters-rights-back-end.herokuapp.com/admin/';
+export const URL='https://renters-rights-back-end.herokuapp.com/admin/';
 
 function Contact() {
-  const [volunteers, setVolunteers] = useState([]);
+  const [volunteers, setVolunteers] = useState();
+  const [schedules, setSchedules] = useState();
+
   useEffect(() => {
     axios 
-      .get(REACT_APP_BACKEND_URL + 'volunteers')
+      .get(URL + 'volunteers')
       .then((res) =>{
         console.log(res.data);
         const newVolunteers = res.data.map((volunteer) => {
           return {
-            volunteer_id: volunteer.volunteerId,
+            volunteerId: volunteer.volunteerId,
             name: volunteer.name,
             email: volunteer.email,
             status: volunteer.status,
             type: volunteer.type,
-            language: volunteer.language
+            language: volunteer.language,
           };
         });
-        setVolunteers(newVolunteers);
         console.log(newVolunteers);
+        setVolunteers(newVolunteers);
+        console.log("hello?")
       })
       .catch((err) => {
         console.log(err);
       });
   },[]);
+
+  const getSchedules= () => {
+    axios.get(URL + 'schedules')
+    .then((res) =>{
+      console.log(res.data);
+      const newSchedules = res.data.map((schedule) => {
+        return {
+          sun: schedule.sun,
+          mon: schedule.mon,
+          tue: schedule.tue,
+          wed: schedule.wed,
+          thu: schedule.thu,
+          fri: schedule.fri,
+          sat: schedule.sat,
+          volunteerId: schedule.volunteerBySchedule.volunteerId,
+          name: schedule.volunteerBySchedule.name
+        };
+      });
+      console.log(newSchedules);
+      setSchedules(newSchedules);
+      console.log("schedules?")
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
   
   return (
     <div>
       <h1>Volunteers</h1>
-      <ul>{volunteers}</ul>
+      <button onClick={getSchedules}>Get Schedules</button>
     </div>
   );
 }
