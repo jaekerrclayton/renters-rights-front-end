2883ee8c0ca017122c5ec797033708924d5e5c94
jae <j.kerr.clayton@gmail.com>
2022-08-03 16:58:56 -0500

    some carousel edits

--- src/components/stage/evictionDocsCarousel.jsx
+++ src/components/stage/evictionDocsCarousel.jsx
@@ -3,13 +3,16 @@ import { Carousel, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from "react-router-dom";
 
+import vacate_1 from './evictionNotices/vacate_1.jpg';
+import court_order from './evictionNotices/court_order.png';
+
 
 
 class EvictionDocuments extends React.Component {
 render() {
 return (
       <div>
-        <div className='container-fluid' >
+        <div >
             <div className="row">
                 <div className="col-sm-12">
                   <h3>Eviction Notices: </h3>
@@ -17,32 +20,38 @@ return (
                   </div>
                   {/* <div className="row">
                   <div className="col-12"> */}
-                  <Carousel showControls interval={10000}>
+                  <Carousel showControls>
                         <Carousel.Item>
+                              <NavLink className="nav-link" to="/eviction-map/1">
+                                      <Button>Notice To Vacate</Button>
+                              </NavLink>
                               <img
                               className="d-block w-100"
-                              src="https://picsum.photos/500/300?img=1"
+                              src={vacate_1}
                               alt="First slide"
                                 />
-                                <Carousel.Caption>
-                                    <h3>First slide label</h3>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                        <NavLink className="nav-link" to="/eviction-map/1">
-                                            <Button variant="primary">Notice To Vacate</Button>
-                                        </NavLink>
-                                </Carousel.Caption>
+                                {/* <Carousel.Caption> */}
+                                    {/* <h3>First slide label</h3> */}
+                                    {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+
+                                {/* </Carousel.Caption> */}
                           </Carousel.Item>
-                                            <Carousel.Item>
-                                            <img
-                                            className="d-block w-100"
-                                            src="https://picsum.photos/500/300?img=2"
-                                            alt="Second slide"
-                                            />
-                                            <Carousel.Caption>
-                                            <h3>Second slide label</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                            </Carousel.Caption>
-                                            </Carousel.Item>
+
+
+                          <Carousel.Item>
+
+                                  <NavLink className="nav-link" to="/eviction-map/2">
+                                      <Button>Court Order</Button>
+                                  </NavLink>
+                                  <img
+                                        className="d-block w-100"
+                                        src={court_order}
+                                        alt="Second slide"
+                                  />
+                                  <Carousel.Caption>
+                                      <h3>Second slide label</h3>
+                                  </Carousel.Caption>
+                          </Carousel.Item>
                                             <Carousel.Item>
                                             <img
                                             className="d-block w-100"
