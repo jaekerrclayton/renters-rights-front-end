e810b63182bb25d0f43e90b6ff46eead4afe6e3b
jae <j.kerr.clayton@gmail.com>
2022-08-08 11:09:51 -0500

    fixed boxes

--- src/components/stage/evictionNotices/order/orderData.jsx
+++ src/components/stage/evictionNotices/order/orderData.jsx
@@ -7,7 +7,7 @@ const orderData = [
     {   id: order1, 
         docInfo: [
 
-      {info_id: 1, 
+      {info_id: 17, 
       title: "Cause of Eviction",
       sub_title: "The order is required to explain the reason you are being evicted", 
       placement: "bottom"
@@ -15,7 +15,7 @@ const orderData = [
     , 
   
     {
-      info_id: 2,
+      info_id: 18,
       title: "Case Number",
       sub_title: "You can call the court at any time to request a translator in advance, or request an extension. Your case number is important in gathering any additional information you may need to provide to the court",
       placement: "bottom"
@@ -23,13 +23,13 @@ const orderData = [
     , 
   
     {
-      info_id: 3, 
+      info_id: 19, 
       title: "Date of Hearing", 
       sub_title: "This is the location and time of your hearing. Check if a room is specified, and prepare to show up early.", 
       placement: "bottom"
     },
   
-    {info_id: 4,
+    {info_id: 20,
     title: "Location of Court", 
     sub_title: "This is the number to call for any questions or continuances",
     placement: "top"
@@ -47,21 +47,21 @@ const orderData = [
     
             [
                     {
-                    info_id: 5,
+                    info_id: 21,
                     title: "",
                     sub_title: "",
                     placement: "",
                 
                     },
                     {
-                    info_id: 6,
+                    info_id: 22,
                     title: "", 
                     sub_title: "",
                     placement: "",
                     
                     },
                     {
-                    info_id: 7,
+                    info_id: 23,
                     title: "",
                     sub_title: "",
                     placement: "",
