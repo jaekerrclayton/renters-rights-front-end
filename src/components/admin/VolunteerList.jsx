444e9c522d7c26d6d24b1a7cbf8afebd44495ea0
Christina Webber <chrisswebb@yahoo.com>
2022-08-08 09:31:54 -0500

    moved schedules to accordion on admin page

--- src/components/admin/VolunteerList.jsx
+++ src/components/admin/VolunteerList.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Volunteer from './Volunteer';
-import Schedule from './Schedule';
+import Accordion from 'react-bootstrap/Accordion';
+import AccordionBody from 'react-bootstrap/esm/AccordionBody';
 
 const Volunteers = (props) => {
     // console.log(typeof props.onEdit);
     const getVolunteers = props.volunteers.map((volunteer) => {
         return (
-            <>
-                <Volunteer
-                        key={volunteer.volunteerId}
-                        volunteerId={volunteer.volunteerId} 
-                        name={volunteer.name} 
-                        email={volunteer.email} 
-                        status={volunteer.status} 
-                        type={volunteer.type}
-                        language={volunteer.language}
-                        schedule={volunteer.schedule}
-                        onEditVolCallback={props.onEdit}
-                        onDeleteVolCallback={props.onDelete}
-                        changeStatus={props.changeStatus}
-                />
-            </>
+            <Accordion.Item eventKey={volunteer.volunteerId}>
+                <Accordion.Header>{volunteer.name}</Accordion.Header>
+                <Accordion.Body>
+                    <Volunteer
+                            key={volunteer.volunteerId}
+                            volunteerId={volunteer.volunteerId}
+                            email={volunteer.email} 
+                            status={volunteer.status} 
+                            type={volunteer.type}
+                            language={volunteer.language}
+                            schedule={volunteer.schedule}
+                            onEditVolCallback={props.onEdit}
+                            onDeleteVolCallback={props.onDelete}
+                            changeStatus={props.changeStatus}
+                    />
+                </Accordion.Body>
+            </Accordion.Item>
         );
     });
 
     return (
-        <div>
-            {getVolunteers}
-        </div>
+        <Accordion>
+                {getVolunteers}
+        </Accordion>
     )
 }
 
