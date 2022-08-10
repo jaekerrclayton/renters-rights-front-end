c3425a767adbac0ce2b2aea859d562883e30501c
jae <j.kerr.clayton@gmail.com>
2022-08-09 19:56:37 -0500

    moved routes into App.js

--- src/index.js
+++ src/index.js
@@ -1,68 +1,14 @@
-import React, {Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {
-  Navigation,
-  Footer,
-  Home,
-  About,
-  Resource,
-  Resources,
-  Contact,
-  Admin, 
-  EvicMap,
-  Stages,
-  Stage,
-  Type,
-  Types,
-} from "./components";
 
 ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="resources" element={<Resources />}>
-            <Route
-              index
-              element={
-                <main style={{ padding: "1rem" }}>
-                </main>
-              }
-            />
-            <Route path=":resourceId" element={<Resource />} />
-          </Route>
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/map-home" element={<EvicMap />}>
-        <Route path="" element={<Stages />} />
-      </Route>
-      <Route path="/eviction-map" element={<Stages />}>
-
-            <Route path="" index />
-            <Route path=":stage_id" element={<Stage />} /> 
-      </Route>
-      <Route path="eviction-types" element={<Types />}>
-          <Route
-              index
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <h2>Select the Cause Given By Your Landlord</h2>
-                  <h3>Need help finding the cause? Look at the examples below.</h3>
-                  <p>**picture examples will go here**</p>
-                </main>
-              }
-            />
-            <Route path=":typeId" element={<Type />} />
-          </Route>
-    </Routes>
-    <Footer />
-  </Router>,
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
