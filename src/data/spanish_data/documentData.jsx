50b9b9b5533f0055b8223495e1d0c04510408171
jae <j.kerr.clayton@gmail.com>
2022-08-11 18:32:55 -0500

    added stg 3

--- src/data/spanish_data/documentData.jsx
+++ src/data/spanish_data/documentData.jsx
@@ -5,6 +5,7 @@ import order1 from '../documents/order1.jpeg';
 import order2 from '../documents/order2.jpeg'; 
 import judgment1 from '../documents/judgment1.jpeg';
 import judgment2 from '../documents/judgment2.jpeg';
+import courtPrep from '../documents/courtPrep.png'; 
 
 
 const documentData = {
@@ -161,6 +162,137 @@ const documentData = {
                     ] 
                 }
     ], 
+    3: [
+
+        {
+            id: courtPrep, 
+            stage_id: 3, 
+            docInfo: [
+
+                {
+
+                    info_id: 24, 
+                    title: "Photographic evidence", 
+                    sub_title: "Bring any photos of your unit you may want to show to the judge", 
+                    placement: "top"
+
+
+                }, 
+                {
+
+                    info_id: 25, 
+                    title: "Text messages", 
+                    sub_title: "Any text messages or written correspondance with your landlord", 
+                    placement: "left"
+
+                }, 
+
+
+                {
+
+                    info_id: 26, 
+                    title: "Witnesses", 
+                    sub_title: "Anyone who can support your side of the story - friend, neighbor, family", 
+                    placement: "left"
+
+                }, 
+
+
+                {
+
+                    info_id: 27, 
+                    title: "All important documents!", 
+                    sub_title: "Bring a copy of your lease if you have one, any reciepts from repairs you may have done, or any evidence that may help you", 
+                    placement: "top"
+
+                }, 
+
+
+
+            ]
+        }
+
+
+
+
+    ],
+
+
+
+    5: 
+    [
+    
+        
+        {   id: judgment1, 
+
+            stage_id: 5,
+            stage_title: "Judgment",  
+            docInfo : [
+                    {info_id: 9,
+                    title: "Your name versus your landlord", 
+                    sub_title: "Check if this information is accurate. Is this the person you pay your lease to ? Consider contacting your landlord to see if there is a way of preventing your eviction from happening. Check out above 'Causes for Evicion' to learn about potential defenses!" ,  
+                    placement: "bottom" 
+                }, 
+                    {
+                    info_id: 10,
+                    title: "Court Location", 
+                    sub_title: "This might look like an official eviciton - but it is just a warning that a hearing could take place. If you do end up facing eviction, this is likely where the court will be held", 
+                    placement: "top" 
+                }, 
+                    {
+                    info_id: 11,
+                    title: "Cause of Eviction" , 
+                    sub_title: "Why you're being evicted - is there something you could do to change the situation ? Consider causes of eviction above, and check out potential defenses." , 
+                    placement: "top" 
+                }
+                    ]
+        } , 
+        {   id: judgment2,
+            stage_id: 5,
+            stage_title: "Judgment",  
+            docInfo: [
+                        {
+                        info_id: 12, 
+                        title: "Time given to respond to eviction",
+                        sub_title: "From the day you have received this notice, the Landlord has given you 5 days to respond. After these five days, the landlord can go to court to request a hearing.", 
+                        placement: "top"
+    
+                        }, 
+                        {
+                        info_id: 13,
+                        title: "Cause for Eviction" , 
+                        sub_title: "Your notice should say why you're being evicted. Look above for different defenses for types of evictions.", 
+                        placement: "bottom"
+                    },
+                    {
+                        info_id: 14, 
+                        title: "Time given to respond to eviction",
+                        sub_title: "From the day you have received this notice, the Landlord has given you 5 days to respond. After these five days, the landlord can go to court to request a hearing.", 
+                        placement: "top"
+    
+                    },
+                    {
+                        info_id: 15, 
+                        title: "Time given to respond to eviction",
+                        sub_title: "From the day you have received this notice, the Landlord has given you 5 days to respond. After these five days, the landlord can go to court to request a hearing.", 
+                        placement: "top"
+    
+                    },
+                    {
+                        info_id: 16, 
+                        title: "Time given to respond to eviction",
+                        sub_title: "From the day you have received this notice, the Landlord has given you 5 days to respond. After these five days, the landlord can go to court to request a hearing.", 
+                        placement: "top"
+    
+                        },  
+                ]   
+        
+        }, 
+        
+        
+    ],
+
+
 
 
 
