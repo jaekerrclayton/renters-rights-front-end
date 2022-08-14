525948da428e9e2e044766c2787483460303d70a
jae <j.kerr.clayton@gmail.com>
2022-08-14 11:11:19 -0500

    more formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import {NavLink} from "react-router-dom"; 
 
 
-
-function Footer({translations}) {
+const Footer = ({translations}) => {
   return (
     <div className="footer">
       <footer id="nav-nav" class="py-sm-0 fixed-bottom">
         {/* <div class="container"> */}
-          <p class="m-0 text-center">
+          <p class="m-1 text-center">
               {/* <p>some text</p> */}
             <NavLink className="nav-link" to="/resources">
                 {translations.footer}
